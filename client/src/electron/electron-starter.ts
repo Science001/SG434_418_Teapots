23f1fa19511b4e9b74af8854efbb85ed3299d083
Balaraman M <balaramanmuthupandi@gmail.com>
2020-08-02 23:20:25 +0530

    Set Node Integration to true in electron starter
    Co-authored-by: S Suryarajan <ssuryarajan@gmail.com>
    Co-authored-by: Ganesh Ramkumar <science.ganesh007@gmail.com>
    Co-authored-by: sriram9159541445@gmail.com <sriram9159541445@gmail.com>

--- client/src/electron/electron-starter.ts
+++ client/src/electron/electron-starter.ts
@@ -12,96 +12,91 @@ import { Subject } from "../entity/subject.entity";
 import { Teacher } from "../entity/teacher.entity";
 import { User } from "../entity/user.entity";
 
-const electron = require('electron');
+const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
-const path = require('path');
-const url = require('url');
-require('dotenv').config();
+const path = require("path");
+const url = require("url");
+require("dotenv").config();
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 function createWindow() {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({ width: 1024, height: 800 });
-    mainWindow.maximize();
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.maximize();
 
-    createConnection({
-        "type": "sqlite",
-        "synchronize": true,
-        "logging": false,
-        "database": `database.sqlite`,
-        "entities": [
-            Academic, Exam, Grade, Location, Posting, Principal, Result, School, Student, Subject, Teacher, User,
-        ]
-    }).then(async connection => {
-        console.log("Connection established to SQLite")
-        const academic = await connection.getRepository(Academic).find();
-        console.log("academic:", academic)
-        const exam = await connection.getRepository(Exam).find();
-        console.log("exam:", exam)
-        const grade = await connection.getRepository(Grade).find();
-        console.log("grade:", grade)
-        const location = await connection.getRepository(Location).find();
-        console.log("location:", location)
-        const posting = await connection.getRepository(Posting).find();
-        console.log("posting:", posting)
-        const principal = await connection.getRepository(Principal).find();
-        console.log("principal:", principal)
-        const result = await connection.getRepository(Result).find();
-        console.log("result:", result)
-        const school = await connection.getRepository(School).find();
-        console.log("school:", school)
-        const student = await connection.getRepository(Student).find();
-        console.log("student:", student)
-        const subject = await connection.getRepository(Subject).find();
-        console.log("subject:", subject)
-        const teacher = await connection.getRepository(Teacher).find();
-        console.log("teacher:", teacher)
-        const user = await connection.getRepository(User).find();
-        console.log("user:", user)
+  createConnection({
+    type: "sqlite",
+    synchronize: true,
+    logging: false,
+    database: "database.sqlite",
+    entities: [
+      Academic,
+      Exam,
+      Grade,
+      Location,
+      Posting,
+      Principal,
+      Result,
+      School,
+      Student,
+      Subject,
+      Teacher,
+      User,
+    ],
+  }).then(async (connection) => {
+    console.log("Connection established to SQLite");
+  });
+
+  // and load the index.html of the app.
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    url.format({
+      pathname: path.join(__dirname, "/../build/index.html"),
+      protocol: "file:",
+      slashes: true,
     });
+  mainWindow.loadURL(startUrl);
 
-    // and load the index.html of the app.
-    const startUrl = process.env.ELECTRON_START_URL || url.format({
-        pathname: path.join(__dirname, '/../build/index.html'),
-        protocol: 'file:',
-        slashes: true
-    });
-    mainWindow.loadURL(startUrl);
-
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null
-    })
+  // Emitted when the window is closed.
+  mainWindow.on("closed", function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+app.on("window-all-closed", function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
-app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow()
-    }
+app.on("activate", function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
 
 // In this file you can include the rest of your app's specific main process
