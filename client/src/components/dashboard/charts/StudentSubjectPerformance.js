73bd711715c9ce388e449185468f0927ba8237c2
Science001 <science.ganesh007@gmail.com>
2020-08-03 16:21:22 +0530

    Final Commit

--- client/src/components/dashboard/charts/StudentSubjectPerformance.js
+++ client/src/components/dashboard/charts/StudentSubjectPerformance.js
@@ -25,31 +25,26 @@ const StudentSubjectPerformance = ({ studentSubjectPerformance }) => {
       style={{
         marginTop: "10em",
       }}
-      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      margin={{ top: 5, right: 30, left: 20, bottom: 20 }}
     >
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis
         dataKey="subject"
         label={{
-          //   value: "District",
+          value: "Subjects",
           color: "#F25022",
           position: "bottom",
         }}
       />
       <YAxis
         label={{
-          value: "No of Schools",
+          value: "Marks",
           angle: -90,
           position: "left",
           color: "#F25022",
         }}
       />
       <Tooltip />
-      <Legend
-        payload={[
-          { value: "Number of Schools", type: "square", color: "#8884d8" },
-        ]}
-      />
       <Bar dataKey="A" barSize={50} fill="#8884d8" />
     </BarChart>
   );
